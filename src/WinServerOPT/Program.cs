94ac2ba354839a5e9099aacf7744f0201bc9e2fe
sheng <sheng@local>
2026-08-15 06:49:14 +0800

    auto-sync: 2026-08-15 06:48

--- src/WinServerOPT/Program.cs
+++ src/WinServerOPT/Program.cs
@@ -5,7 +5,8 @@ static class Program
     [STAThread]
     static void Main()
     {
-        ApplicationConfiguration.Initialize();
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
         Application.Run(new MainForm());
     }
 }
