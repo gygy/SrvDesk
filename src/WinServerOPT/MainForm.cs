14896e9374918e7219e10a8cc80f6c6f7a4dcd43
sheng <sheng@local>
2026-09-06 11:24:16 +0800

    auto-sync: 2026-09-06 11:23

--- src/WinServerOPT/MainForm.cs
+++ src/WinServerOPT/MainForm.cs
@@ -994,7 +994,7 @@ internal sealed class MainForm : Form
         _headerMeter = new HeaderResourceMeter
         {
             Anchor = AnchorStyles.Top | AnchorStyles.Right,
-            Location = new Point(header.Width - 312, 0),
+            Location = new Point(header.Width - 372, 0),
         };
 
         // 蓝色顶栏左侧系统信息，右侧资源占用
